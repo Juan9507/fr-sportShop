02130dcdb9512ea77b2cb4f1acbb3e4ae419ed93
juan9507 <juandavidnaranjo75@gmail.com>
2025-05-02 15:32:37 -0500

    Se crea la página de inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/usuario/login/login.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 const routes: Routes = [
+  {path: 'inicio', component: InicioComponent},
   {path: 'login', component: LoginComponent},
-  {path: '', redirectTo: 'login', pathMatch: 'full'}
+  {path: '', redirectTo: 'inicio', pathMatch: 'full'}
 ];
 
 @NgModule({
---
+++ src/app/components/inicio/inicio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  standalone: false,
+  templateUrl: './inicio.component.html' ,
+  styleUrls: ['./inicio.component.sass']
+})
+export class InicioComponent {
+
+}
